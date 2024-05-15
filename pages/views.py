2ba4e99a04cd96a992784ca62ff3e878897a89bd
ronit16 <ronitshah350@gmail.com>
2024-05-15 11:06:30 +0530

    message board system

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Post
 
 # Create your views here.
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Post
     template_name = 'home.html'
-
-class AboutPageView(TemplateView):
-    template_name = 'about.html'
+    context_object_name = 'all_posts_list'
