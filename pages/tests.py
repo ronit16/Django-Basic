0ca943f876451ee33027a8e4348f3be03478854f
ronit16 <ronitshah350@gmail.com>
2024-05-15 11:21:13 +0530

    message test

--- pages/tests.py
+++ pages/tests.py
@@ -1,11 +1,31 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
+from .models import Post
+from django.urls import reverse
 
 # Create your tests here.
-class SimpleTests(SimpleTestCase):
-    def test_home_page_status_code(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+class PostModelTest(TestCase):
+    
+    def setUp(Self):
+        Post.objects.create(text='just a test')
 
-    def test_about_page_status_code(self):
-        response = self.client.get('/about/')
-        self.assertEqual(response.status_code, 200)
+    def test_text_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.text}'
+        self.assertEqual(expected_object_name, 'just a test')
+
+class HomePageViewTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text='this is another text')
+
+    def test_view_url_exists_at_proper_location(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'home.html')
